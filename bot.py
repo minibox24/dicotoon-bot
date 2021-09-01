71fb932811ee828b6427bdd292c26ee95084fa2b
minibox24 <minibox724@gmail.com>
2021-09-01 18:11:27 +0900

    ✨ DB

--- bot.py
+++ bot.py
@@ -1,11 +1,18 @@
 from discord.ext.commands import Bot
+from tortoise import Tortoise
+
 
 class DicoToon(Bot):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, db_url, *args, **kwargs):
         if not kwargs.get("command_prefix"):
             kwargs["command_prefix"] = "!"
 
         super().__init__(*args, **kwargs)
 
+        self.db_url = db_url
+
     async def on_ready(self):
-        print('ready')
+        await Tortoise.init(db_url=self.db_url, modules={"models": ["models"]})
+        await Tortoise.generate_schemas()
+
+        print("ready")
--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from bot import DicoToon
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+DB_URL = os.environ.get("DB_URL")
 
-bot = DicoToon()
+bot = DicoToon(DB_URL)
 
 bot.run(BOT_TOKEN)
