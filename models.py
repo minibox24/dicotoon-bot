00bd7fe2ebd7315bb3e516891f68b3ec5e86f23f
minibox24 <minibox724@gmail.com>
2021-09-01 18:07:32 +0900

    ✨ 모델

---
+++ models.py
@@ -0,0 +1,28 @@
+from tortoise.models import Model
+from tortoise import fields
+
+
+class ImageURLField(fields.TextField):
+    def to_db_value(self, value, _):
+        return value.split("/attachments/")[1]
+
+    def to_python_value(self, value):
+        return f"https://cdn.discordapp.com/attachments/{value}"
+
+
+class ToonUser(Model):
+    id = fields.BigIntField(pk=True)
+    name = fields.TextField()
+    avatar = fields.TextField()
+
+
+class ToonChannel(Model):
+    id = fields.BigIntField(pk=True)
+    name = fields.TextField()
+
+
+class ToonData(Model):
+    id = fields.IntField(pk=True)
+    url = ImageURLField()
+    user = fields.OneToOneField("models.ToonUser")
+    channel = fields.OneToOneField("models.ToonChannel")
