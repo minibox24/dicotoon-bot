6ca62b20beeb82a366c12bb69a436cd0541b1767
minibox24 <minibox724@gmail.com>
2021-09-01 18:23:37 +0900

    ✨ ConfirmView 클래스

--- dicotoon.py
+++ dicotoon.py
@@ -1,4 +1,30 @@
+import discord
 from discord.ext import commands
+from discord.ui.view import View
+from discord.ui.button import button
+
+
+class ConfirmView(View):
+    def __init__(self, user_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.user_id = user_id
+        self.confirm = False
+
+    @button(label="네", style=discord.ButtonStyle.green)
+    async def yes(self, _, interaction):
+        if interaction.user.id != self.user_id:
+            return
+
+        self.confirm = True
+        self.stop()
+
+    @button(label="아니오", style=discord.ButtonStyle.red)
+    async def no(self, _, interaction):
+        if interaction.user.id != self.user_id:
+            return
+
+        self.stop()
 
 
 class DicoToonCog(commands.Cog):
